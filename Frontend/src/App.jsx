ae9e8e85e5272322cd1d7a37b228460400cfa63f
pawanharry <wazirpawandeep23@gmail.com>
2024-07-10 02:48:05 -0400

    footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Services from './components/Services'
+import Footer from './components/Footer'
 
 
 
@@ -13,6 +14,7 @@ const App = () => {
     <Hero/>
     <About/>
     <Services/>
+    <Footer/>
     </>
   )
 }
